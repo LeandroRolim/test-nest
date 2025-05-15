533f1e19a71f21b596e673f43f840538c03fcaf8
Leandro Rolim <leandro@lrconsultoria.tec.br>
2025-05-15 10:45:11 -0300

    Generate Prisma client files for database models.
    Added auto-generated Prisma client files for interacting with the database. These include models for `Product`, `Order`, and `OrderProduct` with defined relationships and schema configurations.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProductsModule } from './products/products.module';
+import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, ProductsModule],
+  controllers: [AppController, ProductsController],
+  providers: [AppService, ProductsService],
 })
 export class AppModule {}
