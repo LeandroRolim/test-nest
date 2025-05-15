59c3f9db5d7d83b06aba5057c4aabc946bb9f2b3
Leandro Rolim <leandro@lrconsultoria.tec.br>
2025-05-15 13:48:24 -0300

    Add LoggerMiddleware for HTTP request logging
    Introduced a LoggerMiddleware to log HTTP request details, including method, URL, status code, content length, user agent, and IP address. Configured the middleware in `AppModule` to apply globally to all routes.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
@@ -8,10 +8,15 @@ import { ProductsService } from './products.service';
 import { OrdersModule } from './orders/orders.module';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
   imports: [PrismaModule, ProductsModule, OrdersModule],
   controllers: [AppController, ProductsController, OrdersController],
   providers: [AppService, ProductsService, OrdersService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
