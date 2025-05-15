bd12f7aded3a80d584d87c9b019352620b9ec5c3
Leandro Rolim <leandro@lrconsultoria.tec.br>
2025-05-15 14:35:28 -0300

    Refactor: relocate controllers and services to feature modules
    Moved `ProductsController`, `ProductsService`, `OrdersController`, and `OrdersService` to their respective feature directories for better organization. Updated imports in `app.module.ts` to reflect these changes.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
-import { ProductsController } from './products.controller';
-import { ProductsService } from './products.service';
 import { OrdersModule } from './orders/orders.module';
-import { OrdersController } from './orders.controller';
-import { OrdersService } from './orders.service';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { ProductsController } from './products/products.controller';
+import { OrdersController } from './orders/orders.controller';
+import { OrdersService } from './orders/orders.service';
+import { ProductsService } from './products/products.service';
 
 @Module({
   imports: [PrismaModule, ProductsModule, OrdersModule],
