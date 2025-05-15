40132ee6c9e928df06505be45a66068cf63fe05f
Leandro Rolim <leandro@lrconsultoria.tec.br>
2025-05-15 14:01:32 -0300

    Refactor @ApiProperty annotations in ProductEntity
    Reformatted @ApiProperty annotations to improve readability and maintain consistency. This change ensures the code is cleaner and easier to update in the future.

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -2,28 +2,51 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Product as PrismaProduct } from '@prisma/client';
 
 export class ProductEntity implements PrismaProduct {
-  @ApiProperty({ example: 'a1b2c3d4-e5f6-7890-1234-567890abcdef', description: 'The unique identifier of the product' })
+  @ApiProperty({
+    example: 'a1b2c3d4-e5f6-7890-1234-567890abcdef',
+    description: 'The unique identifier of the product',
+  })
   id: string;
 
   @ApiProperty({ example: 'Laptop Pro', description: 'Name of the product' })
   name: string;
 
-  @ApiProperty({ example: 'Electronics', description: 'Category of the product' })
+  @ApiProperty({
+    example: 'Electronics',
+    description: 'Category of the product',
+  })
   category: string;
 
-  @ApiProperty({ example: 'High-performance laptop for professionals', description: 'Detailed description of the product' })
+  @ApiProperty({
+    example: 'High-performance laptop for professionals',
+    description: 'Detailed description of the product',
+  })
   description: string;
 
-  @ApiProperty({ example: 1200.99, type: Number, description: 'Price of the product' })
+  @ApiProperty({
+    example: 1200.99,
+    type: Number,
+    description: 'Price of the product',
+  })
   price: number;
 
-  @ApiProperty({ example: 50, type: Number, description: 'Available quantity in stock' })
+  @ApiProperty({
+    example: 50,
+    type: Number,
+    description: 'Available quantity in stock',
+  })
   stock_quantity: number;
 
-  @ApiProperty({ example: '2023-01-15T10:30:00.000Z', description: 'Timestamp of when the product was created' })
+  @ApiProperty({
+    example: '2023-01-15T10:30:00.000Z',
+    description: 'Timestamp of when the product was created',
+  })
   createdAt: Date;
 
-  @ApiProperty({ example: '2023-01-16T12:45:00.000Z', description: 'Timestamp of when the product was last updated' })
+  @ApiProperty({
+    example: '2023-01-16T12:45:00.000Z',
+    description: 'Timestamp of when the product was last updated',
+  })
   updatedAt: Date;
 
   // Construtor opcional para facilitar a criação da entidade a partir de um objeto Prisma
